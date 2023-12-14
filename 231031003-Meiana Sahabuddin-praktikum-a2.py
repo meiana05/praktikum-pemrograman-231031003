32e4914b2c2a00c07cc9114f613eef57182e582f
meiana05 <144907967+meiana05@users.noreply.github.com>
2023-12-14 05:58:44 -0800

    praktikum-a2
    Operator Assingment, Perbandingan, Logika, dan Membership

---
+++ 231031003-Meiana Sahabuddin-praktikum-a2.py
@@ -0,0 +1,292 @@
+print('pratikum-a2\n')
+
+print('Nama    : Meiana sahabuddin')
+print('Nim     : 231031003')
+print('Prodi   : Sistem informasi A\n')
+
+# INI OPERATOR ASSIGMENT
+a = 19
+print('Nilai a adalah',a)
+a +=3
+print('Nilai a A+=3 adalah',a)
+
+a = 19
+print('Nilai a adalah',a)
+a -=3
+print('Nilai a -=3 adalah',a)
+
+a = 19
+print('Nilai a adalah',a)
+a *=2
+print('Nilai a *=2 adalah',a)
+
+a = 19
+print('Nilai a adalah',a)
+a /=2
+print('Nilai a /=2 adalah',a)
+
+a = 3
+print('Nilai a adalah',a)
+a **=2
+print('Nilai a **=2 adalah',a)
+
+a = 35
+print('Nilai a adalah',a)
+a %=32
+print('Nilai a %=32 adalah',a)
+
+a = 35
+print('Nilai a adalah',a)
+a //=32
+print('Nilai a //=32 adalah',a)
+
+# Tugas selanjutnya untuk operator selanjutnya
+
+
+# OPERATOR PERBANDINGAN
+a =9
+p =13
+print('\n________ Besar dari 13')
+hasil = a>13
+print(a,'>13 adalah',hasil)
+b =13
+p =13
+print('\n________ Besar dari 13')
+hasil = b>13
+print(b,'>13 adalah',hasil)
+
+a =9
+p =3
+print('\n________ Besar dari 13')
+hasil = a<13
+print(a,'<13 adalah',hasil)
+b =13
+p =13
+print('\n________ Besar dari 13')
+hasil = b<3
+print(b,'<3 adalah',hasil)
+
+a =9
+p =3
+print('\n________ Besar dari 13')
+hasil = a<3
+print(a,'<3 adalah',hasil)
+b =13
+p =3
+print('\n________ Besar dari 13')
+hasil = b<3
+print(b,'<3 adalah',hasil)
+
+a= 3
+b= 3
+print('\n------Besar atau sama dari 03')
+hasil = a >= 3
+print(a,'>= 3 adalah',hasil)
+hasil = b >= 3
+print(b,'>= 3 adalah',hasil)
+
+a= 3
+b= 3
+print('\n------Kecil atau sama dari 03')
+hasil = a <= 3
+print(a,'<= 3 adalah',hasil)
+hasil = b <= 3
+print(b,'<= 3 adalah',hasil)
+
+a= 3
+b= 3
+print('\n------Sama dari 03')
+hasil = a == 3
+print(a,'== 3 adalah',hasil)
+hasil = b == 3
+print(b,'== 3 adalah',hasil)
+
+a= 3
+b= 3
+print('\n------Tidak sama dari 03')
+hasil = a != 3
+print(a,'!= 3 adalah',hasil)
+hasil = b != 3
+print(b,'!= 3 adalah',hasil)
+
+#operator logika
+a= True
+b= False
+print('\n-------AND-------')
+hasil = a and a
+print(a,'and',a,'hasilnya =',hasil)
+hasil = a and b
+print(a,'and',b,'hasilnya =',hasil)
+hasil = b and a
+print(b,'and',a,'hasilnya =',hasil)
+hasil = b and b
+print(b,'and',b,'hasilnya =',hasil)
+
+print('\n---------OR-------')
+hasil = a or a
+print(a,'or',a,'hasilnya =',hasil)
+hasil = a or b
+print(a,'or',b,'hasilnya =',hasil)
+hasil = b or a
+print(b,'or',a,'hasilnya =',hasil)
+hasil = b or b
+print(b,'or',b,'hasilnya =',hasil)
+
+
+print('\n---------XOR-------')
+hasil = a ^ a
+print(a,'xor',a,'hasilnya =',hasil)
+hasil = a ^ b
+print(a,'xor',b,'hasilnya =',hasil)
+hasil = b ^ a
+print(b,'xor',a,'hasilnya =',hasil)
+hasil = b ^ b
+print(b,'xor',b,'hasilnya =',hasil)
+
+print('\n---------NOT-------')
+hasil = not a
+print('not',a,'hasilnya =',hasil)
+hasil = not b
+print('not',b,'hasilnya =',hasil)
+
+
+#operator membership
+print('\n==========IN')
+nama= 'meiana'
+
+test= 'ia'
+cek= test in nama
+print(test,'terdapat di',nama,'adalah',cek)
+
+test= 'ai'
+cek= test in nama
+print(test,'terdapat di',nama,'adalah',cek)
+
+test1 = 'a'
+test2 = 'i'
+test3 = 'u'
+test4 = 'e'
+test5 = 'o'
+
+test1 = 'a'
+cek= test1 in nama
+print(test1,'terdapat di',nama,'adalah',cek)
+
+test2 = 'i'
+cek= test2 in nama
+print(test2,'terdapat di',nama,'adalah',cek)
+
+test3 = 'u'
+cek= test3 in nama
+print(test3,'terdapat di',nama,'adalah',cek)
+
+test4 = 'e'
+cek= test4 in nama
+print(test4,'terdapat di',nama,'adalah',cek)
+
+test5 = 'o'
+cek= test5 in nama
+print(test5,'terdapat di',nama,'adalah',cek)
+
+
+print('\n============NOT IN')
+#tugas lanjutan untuk not in dengan cara yang sama
+nama= 'meiana'
+
+test= 'ia'
+cek= test not in nama
+print(test,'terdapat di',nama,'adalah',cek)
+
+test= 'ai'
+cek= test not in nama
+print(test,'terdapat di',nama,'adalah',cek)
+
+test1 = 'a'
+test2 = 'i'
+test3 = 'u'
+test4 = 'e'
+test5 = 'o'
+
+test1 = 'a'
+cek= test1 not in nama
+print(test1,'terdapat di',nama,'adalah',cek)
+
+test2 = 'i'
+cek= test2 not in nama
+print(test2,'terdapat di',nama,'adalah',cek)
+
+test3 = 'u'
+cek= test3 not in nama
+print(test3,'terdapat di',nama,'adalah',cek)
+
+test4 = 'e'
+cek= test4 not in nama
+print(test4,'terdapat di',nama,'adalah',cek)
+
+test5 = 'o'
+cek= test5 not in nama
+print(test5,'terdapat di',nama,'adalah',cek)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

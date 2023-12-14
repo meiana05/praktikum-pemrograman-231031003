a9ce3bcbd4a485a7c600cba6276df9587155ba35
meiana05 <144907967+meiana05@users.noreply.github.com>
2023-12-14 07:18:25 -0800

    latihan1
    Mendefinisikan nilai dan batas lulus

---
+++ 231031003-Meiana Sahabuddin-latihan1.py
@@ -0,0 +1,10 @@
+#Mendefinisikan nilai dan batas lulus
+nilai = 70
+nilai = float(input(' Masukkan Nilai: '))
+batas_nilai = 65
+#Mengecek apakah nilai lebih besar dari batas lulus
+if nilai >=batas_nilai:
+    print('Selamat, Kamu lulus!')
+else:
+     print('Sayang sekali, kamu tidak lulus!')
+

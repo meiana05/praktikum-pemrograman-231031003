b37882d48e5fc409736f4df3609189979c85b364
meiana05 <144907967+meiana05@users.noreply.github.com>
2023-12-14 07:19:36 -0800

    latihan2
    Menerima input dari pengguna

---
+++ 231031003-Meiana Sahabuddin-latihan2.py
@@ -0,0 +1,8 @@
+#Menerima input dari pengguna
+inp = input('Masukkan Suatu input: ')
+#Mengecek panjang input
+if len(inp) !=3:
+    print('Panjang string ', len(inp),'tidak sama dengan 3')
+else:
+ print('Panjang input sesuai yang diinginkan')
+

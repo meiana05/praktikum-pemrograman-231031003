8aa7d7c4767484c9d81fc3f4a676f37d0daa1dd8
meiana05 <144907967+meiana05@users.noreply.github.com>
2023-12-14 06:28:54 -0800

    praktikum-a5
    Menginisialisasi sebuah variabel boolean

---
+++ 231031003-Meiana Sahabuddin-praktikum-a5.py
@@ -0,0 +1,15 @@
+# File: program.py 
+
+#Menginisialisasi sebuah variabel boolean 'a' dengan nilai 'True','count=0': Menginisialisasi sebuah variabel penghitung 'count' dengan nilai '0'
+a = True
+count = 0
+
+#While a Memulai perulangan sementara yang terus berlanjut selama kondisi `a` bernilai benar.`print('menjalankan program')`: Mencetak pesan yang ditentukan.
+while a:
+        print('Menjalankan Program')
+        count += 1
+        if count == 5:
+            break
+
+#`count += 1`: Menambah penghitung sebanyak 1 di setiap iterasi.
+# `jika hitungan == 5: istirahat`: Memeriksa apakah penghitung telah mencapai 5 dan keluar dari perulangan jika benar.

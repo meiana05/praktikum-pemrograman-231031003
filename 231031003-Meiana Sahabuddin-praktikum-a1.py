ec4f3bf9451f2eb15a921fdbbfc84d1524667e40
meiana05 <144907967+meiana05@users.noreply.github.com>
2023-12-14 05:50:24 -0800

    pratikum-a1
    Bahasa Python dan Tipe data dasar

---
+++ 231031003-Meiana Sahabuddin-praktikum-a1.py
@@ -0,0 +1,91 @@
+print('Nama lengkap: Meiana Sahabuddin')
+print('Nim         : 231031003')
+print('Prodi       : Sistem Informasi')
+print()
+x=2023
+print('Angkatan'+' '+str(x))
+print()
+
+a=10
+print('Data adalah: ',a)
+print('tipe data adalah',type(a))
+print()
+
+b=10.0
+print('Data adalah: ',b)
+print('tipe data adalah',type(b))
+print()
+
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah: ',kampus)
+print('tipe data kampus adalah',type(kampus))
+
+c=complex(2,3)
+print('Data adalah: ',c)
+print('tipe data adalah',type(c))
+print()
+
+log = True
+print('Data adalah: ',log)
+print('tipe data adalah',type(log))
+print()
+ 
+p=20
+q=23
+hasil= p + q
+print('hasil',p,'+',q,'=',hasil)
+print('Ini adalah batas untuk tugas praktikum###')
+print()
+
+print('Tugas Operator Aritmatika')
+print()
+
+m=20
+e=10
+hasil=m+e
+print('Penjumlahan')
+print('hasil',m,'+',e,'=',hasil)
+print()
+
+m=25
+e=10
+hasil=m-e
+print('Pengurangan')
+print('hasil',m,'-',e,'=',hasil)
+print()
+
+m=20
+e=2
+hasil=m*e
+print('Perkalian')
+print('hasil',m,'*',e,'=',hasil)
+print()
+
+m=20
+e=5
+hasil=m/e
+print('Pembagian')
+print('hasil',m,'/',e,'=',hasil)
+print()
+
+m=23
+e=5
+hasil=m%e
+print('Modulus')
+print('hasil',m,'%',e,'=',hasil)
+print()
+
+m=25
+se=3
+hasil=m**e
+print('Eksponensial')
+print('hasil',m,'**',e,'=',hasil)
+print()
+
+m=20
+e=5
+hasil=m//e
+print('Floor Division')
+print('hasil',m,'//',e,'=',hasil)
+print()
+
